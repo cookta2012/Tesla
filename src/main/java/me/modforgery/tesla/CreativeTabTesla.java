b5159880f9e7eeac2c2fe9e0334287f3937e527b
Rushmead <Rushmead@techie.com>
2014-08-31 16:46:27 +0100

    CreativeTab: Added

---
+++ src/main/java/me/modforgery/tesla/CreativeTabTesla.java
@@ -0,0 +1,19 @@
+package me.modforgery.tesla;
+
+import me.modforgery.tesla.init.TeslaItems;
+import me.modforgery.tesla.reference.Reference;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+/**
+ * Created by Rushmead For Tesla
+ */
+public class CreativeTabTesla {
+    public static final CreativeTabs TS_TAB = new CreativeTabs(Reference.modid.toLowerCase()) {
+        @Override
+        public Item getTabIconItem() {
+            return TeslaItems.tp_device;
+        }
+    };
+
+}
