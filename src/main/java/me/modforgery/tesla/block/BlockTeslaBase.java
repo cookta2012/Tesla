afdc84dee6d84c79d58e6a4064ce7bba1d97fb15
modmuss50 <m123world@gmail.com>
2014-09-01 18:28:03 +0100

    Fixes the tile entity not getting removed
    @tattyseal there is a reason to extend that, we can return null if we
    get a base block class

--- src/main/java/me/modforgery/tesla/block/BlockTeslaBase.java
+++ src/main/java/me/modforgery/tesla/block/BlockTeslaBase.java
@@ -23,4 +23,25 @@ public class BlockTeslaBase extends Block implements ITileEntityProvider
     public TileEntity createNewTileEntity(World p_149915_1_, int p_149915_2_) {
         return null;
     }
+
+    /**
+     * Called whenever the block is added into the world. Args: world, x, y, z
+     */
+    public void onBlockAdded(World world, int x, int y, int z)
+    {
+        super.onBlockAdded(world, x, y, z);
+    }
+
+    public void breakBlock(World world, int x, int y, int z, Block block, int meta)
+    {
+        super.breakBlock(world, x, y, z, block, meta);
+        world.removeTileEntity(x, y, z);
+    }
+
+    public boolean onBlockEventReceived(World world, int x, int y, int z, int p_149696_5_, int p_149696_6_)
+    {
+        super.onBlockEventReceived(world, x, y, z, p_149696_5_, p_149696_6_);
+        TileEntity tileentity = world.getTileEntity(x, y, z);
+        return tileentity != null ? tileentity.receiveClientEvent(p_149696_5_, p_149696_6_) : false;
+    }
 }
